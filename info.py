e8d2b277e7c684c135d182dc96869d6818638c48
virus-botz <158062548+virus-botz@users.noreply.github.com>
2024-04-06 22:47:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID','22549633'))
-API_HASH = environ.get('API_HASH','34d8c9887fe445c1dac2228cbdf9ab48)
+API_HASH = environ.get('API_HASH','34d8c9887fe445c1dac2228cbdf9ab48')
 BOT_TOKEN = environ.get('BOT_TOKEN','')
 
 # Bot settings
